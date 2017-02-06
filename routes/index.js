eb0021bc45ad4275ff02a28b62396286e969b2ff
Alic Szecsei <aszecsei@gmail.com>
2017-02-06 17:45:54 -0600

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'ThreeBee', body: 'We be three bees! Alic, Emma, and Tanner.' });
+});
+
+module.exports = router;

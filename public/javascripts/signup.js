c9c28286e09e72683fe96f0173ba20b15a2fc706
Tanner <tanner-brunscheon@uiowa.edu>
2017-02-07 22:05:52 -0600

    Cant Figure out why signupScripts keeps 404ing

---
+++ public/javascripts/signup.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Tanner on 2/7/2017.
+ */
+function submit() {
+        document.getElementById("lastName").value = document.getElementById("firstName").value;
+}
+
--- src/signupScripts.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Tanner on 2/7/2017.
- */
-function submit() {
-
-    document.getElementById("lastName").value = document.getElementById("firstName").value;
-
-}

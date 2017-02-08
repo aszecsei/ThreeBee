aeafb8426953d6fd663505b3f90018117179297a
Alic Szecsei <aszecsei@gmail.com>
2017-02-08 12:31:28 -0600

    Added unit tests for passwords and index page retrieval

--- test/index.js
+++ test/index.js
@@ -2,3 +2,26 @@
 
 'use strict';
 
+var expect    = require("chai").expect;
+var request = require("request");
+var User = require("../src/models/user");
+
+describe("Web", function() {
+    it("Index returns status 200", function () {
+        var url = "http://localhost:3000/";
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+});
+
+describe("User", function() {
+    describe("Password Hashing", function() {
+        it("Hashes passwords match", function() {
+            var user = new User();
+            user.password = user.generateHash("password");
+            expect(user.validPassword("password"));
+        });
+    });
+});

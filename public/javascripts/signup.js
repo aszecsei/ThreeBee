a1f2134cf50de40b34c536d6b101792c453d398a
Alic Szecsei <aszecsei@gmail.com>
2017-02-13 18:10:56 -0600

    Added HTML5 code validation

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -2,21 +2,27 @@
  * Created by Tanner on 2/7/2017.
  */
 var invalid = "~`!#$%^&*+=-[]\\\';,/{}|\":<>?";
-function submit() {
-        checkFirst()
+
+function testInvalid(input) {
+    for(var i=0; i<input.length; i++) {
+        if(invalid.includes(input.charAt(i))) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function checkFirst() {
     var first = document.getElementById("firstName").value;
-    if (first.test(invalid)){
+    console.log("Checking...");
+    if (testInvalid(first)){
             return false;
     }
-    else if(!first.test(invalid)){
+    else {
             return true;
     }
-
-
 }
+
 function checkLast() {
 
     var last = document.getElementById("lastName").value;
@@ -28,6 +34,7 @@ function checkLast() {
     }
 
 }
+
 function checkEmail() {
     var first = document.getElementById("firstName").value;
     if (first.test(invalid)){
@@ -54,3 +61,8 @@ function checkCountry() {
 function checkZip() {
 
 }
+
+// When the document is ready
+$(function() {
+
+});

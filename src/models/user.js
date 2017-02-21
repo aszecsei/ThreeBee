a69e4422ea7edc0a87a7e588f6670520c1e32514
Alic Szecsei <aszecsei@gmail.com>
2017-02-20 19:35:13 -0600

    Fixed up password generation

--- src/models/user.js
+++ src/models/user.js
@@ -19,44 +19,32 @@ function User(id, email, password, userType) {
         return bcrypt.compareSync(password, this.password);
     };
     this.generateTempPass = function() {
-        var password = [];
-        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=';
-        for(var j=1;j<5;j++) {
-            password[j] = ""
-            for (var i = 1; i < 5; i++) {
-                var c = Math.floor(Math.random() * chars.length + 1);
-                password[j] += chars.charAt(c)
-            }
-        }
-        while((password[1].search('/[A-Z]/') !==-1)){
-            for (var i = 1; i < 5; i++) {
-                var c = Math.floor(Math.random() * chars.length + 1);
-                password[1] += chars.charAt(c)
-            }
-        }
-        while((password[2].search('/[a-z]/') !==-1)){
-            for (var i = 1; i < 5; i++) {
-                var c = Math.floor(Math.random() * chars.length + 1);
-                password[2] += chars.charAt(c)
-            }
-        }
-        while((password[1].search('/[0-9]/') !==-1)){
-            for (var i = 1; i < 5; i++) {
-                var c = Math.floor(Math.random() * chars.length + 1);
-                password[1] += chars.charAt(c)
-            }
-        }
-        while((password[1].search('/[-!$%^&*()_+|~=`{}[]:/;<>?,.@#]/') !==-1)){
-            for (var i = 1; i < 5; i++) {
-                var c = Math.floor(Math.random() * chars.length + 1);
-                password[1] += chars.charAt(c)
-            }
-        }
-        var passwordEnd = password[1]+password[2]+password[3]+password[4];
+        const lowChars = "abcdefghijklmnopqrstuvwxyz".split("");
+        const capChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+        const numChars = "0123456789".split("");
+        const spcChars = "~!@#$%^&*_-+=?".split("");
+        const allChars = lowChars.concat(capChars).concat(numChars).concat(spcChars); // Create arrays of each type of character
 
+        var strings =  [lowChars[Math.floor(Math.random() * lowChars.length)],
+                        capChars[Math.floor(Math.random() * capChars.length)],
+                        numChars[Math.floor(Math.random() * numChars.length)],
+                        spcChars[Math.floor(Math.random() * spcChars.length)]]; // Create 4 strings, each of which has a required character type
 
+        var selection;
 
-        return passwordEnd;
+        for(var i=0; i<16; i++) {
+            selection = Math.floor(Math.random() * strings.length); // Select a string to alter
+            strings[selection] = (Math.random() > 0.5) ?
+                (strings[selection] + allChars[Math.floor(Math.random() * allChars)]) :
+                allChars[Math.floor(Math.random() * allChars)] + strings[selection]; // Randomly append or prepend a random character to our selected string
+        }
+        var result = "";
+        while(strings.length > 0) {
+            selection = Math.floor(Math.random() * strings.length); // Select a string to add
+            result += strings[selection]; // Add it to our result
+            strings.splice(selection, 1); // Remove the selected string from the array of strings
+        }
+        return result;
     };
     this.save = function (callback) {
         return db.run("INSERT OR IGNORE INTO USER (email, password, user_type) VALUES (?,?,?)", [this.email, this.password, this.user_type],  function(err) {

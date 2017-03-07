7c76a546187e958cbf738d2cf4fccb7e02125616
Tanner <tanner-brunscheon@uiowa.edu>
2017-03-06 22:33:01 -0600

    Fixing planes post

--- routes/planes.js
+++ routes/planes.js
@@ -4,14 +4,17 @@ var express = require('express');
 var router = express.Router();
 
 var passport = require('passport');
-var Plane = require(''../src/models/planes'')
+var Plane = require('./src/models/planes');
 
 router.get('/', function(req, res, next) {
     res.render('planes', {shouldDisplayLogin: 2});
 });
 
 router.post('/', function(req, res) {
-    Plane.FindOne
+    Plane.findOne({ 'name' :  req.body.name}, function(err, user){
+
+
+    });
 });
 
 

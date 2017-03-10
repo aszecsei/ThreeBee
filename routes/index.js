9dd1b4baef022b8bed35bc5ed65b2a553c41ff68
Alic Szecsei <aszecsei@gmail.com>
2017-03-10 13:51:27 -0600

    Check for login in index page

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  if(req.user.user_type == 1 && req.user.auth_status == 0) {
-    res.redirect('/signupmanager/changepassword');
+  if(req.user) { // check if we're logged in
+      if (req.user.user_type == 1 && req.user.auth_status == 0) {
+          res.redirect('/signupmanager/changepassword');
+      }
   }
 
   res.render('index', { title: 'ThreeBee', body: 'We be three bees! Alic, Emma, and Tanner.', shouldDisplayLogin: (req.isAuthenticated() ? 1 : 0) });

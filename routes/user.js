b97710e4f56c6c83a46df72f9a7bea32abd91911
Alic Szecsei <aszecsei@gmail.com>
2017-03-29 17:23:55 -0500

    Allow users to change their passwords

--- routes/user.js
+++ routes/user.js
@@ -11,26 +11,37 @@ var email = require('../src/email');
 
 var auth = require('../src/auth');
 
+var errorHandle = function(res, message) {
+    res.status(500).json(message);
+};
+
 /* GET manager change password page. */
 router.get('/changepassword', function(req, res) {
     if(req.user)
-        res.render('managerchangepassword', {shouldDisplayLogin: 2});
+        res.render('changepassword', {shouldDisplayLogin: 1, required: req.user.user_type == 1 && req.user.auth_status == 0});
     else
         res.redirect("/");
 });
 
 router.post('/changepassword', function(req, res) {
-    if(req.user.user_type == 1 && req.user.auth_status == 0) {
-        var usr = req.user;
-        usr.password = usr.generateHash(req.body.password);
-        usr.auth_status = 1;
-        usr.update(function(err, id) {
-            if(err)
-                throw err;
-            res.redirect("/");
-        });
+    var usr = new User();
+    if(usr.checkPass(req.body.password)) {
+        if (req.user && ((req.user.user_type == 1 && req.user.auth_status == 0) || req.user.auth_status == 1)) {
+            usr = req.user;
+            usr.password = usr.generateHash(req.body.password);
+            usr.auth_status = 1;
+            usr.update(function (err, id) {
+                if (err) {
+                    errorHandle(res, err);
+                } else {
+                    res.json({message: 'Password changed'});
+                }
+            });
+        } else {
+            errorHandle(res, "You are not authorized to perform this action.")
+        }
     } else {
-        res.redirect("/");
+        errorHandle(res, "Passwords must be at least 8 characters long, and must contain at least one uppercase letter, lowercase letter, and letter.");
     }
 });
 

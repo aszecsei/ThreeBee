49193e5e4f278314640041eb147ee504e181c45f
Tanner <tanner-brunscheon@uiowa.edu>
2017-04-03 20:06:04 -0500

    Booking model started

--- src/models/booking.js
+++ src/models/booking.js
@@ -28,40 +28,8 @@ function Booking(id, flightID, userID,type) {
     };
 }
 Booking.delete = function (id, callback) {
-    db.query("UPDATE `threebee`.`flight_data` SET `flight_isActive`='0' WHERE flightID = '" +id+"';");
+    db.query("UPDATE `threebee`.`bookings` SET `isActive`='0' WHERE bookingid = '" +id+"';");
     callback();
-};
-Booking.deletePlane = function (id, callback) {
-    db.query("UPDATE `threebee`.`flight_data` SET `flight_isActive`='0' WHERE planeID = '" +id+"';");
-    callback();
-};
-Booking.findOne = function (params, callback) {
-    // create the array
-    var stringArray = [];
-    var paramArray = [];
-    for (var prop in params) {
-        if (params.hasOwnProperty(prop)) {
-            stringArray.push("?=?");
-            paramArray.push(prop);
-            paramArray.push(params[prop]);
-        }
-    }
-    db.query("SELECT * FROM threebee.flight_data WHERE (" + stringArray.join(" AND ") + ")", paramArray, function (err, row) {
-        if (err) {
-            callback(err, undefined);
-            return;
-        }
-        if (row.length > 0) {
-            var result = new Flight(row[0].idflight_data, row[0].name, row[0].numFirstSeat, row[0].numBizSeat, row[0].numCoachSeat);
-            callback(err, result);
-            return;
-        }
-        callback(err, undefined);
-    });
-};
-
-Booking.findById = function (id, callback) {
-    return Flight.findOne({id: id}, callback);
 };
 
 Booking.query = function (callback) {

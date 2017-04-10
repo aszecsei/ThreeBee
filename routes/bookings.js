373bcafbce32a637fea27ccc48f96d2019c3f4c6
Tanner <tanner-brunscheon@uiowa.edu>
2017-04-09 23:10:19 -0500

    Setting up booking good enough for tommorow

--- routes/bookings.js
+++ routes/bookings.js
@@ -7,50 +7,28 @@ var auth = require('../src/auth');
 var db = require('../src/database');
 var Booking = require('../src/models/booking');
 var Flight = require('../src/models/flight')
+var async = require('async');
 router.get('/', function(req, res) {
-    console.log("Test In");
-    var toPass = [];
-    Booking.findHead(req.passport.user.id, function(err, row) {
-        console.log("Test 1");
-        for (var i = 0; i < row.length; i++) {
-            console.log(i);
-            if (row[i].nextBook != 0) {
-                console.log(i);
-                console.log((row[i].nextBook));
-                var j = i
-                Booking.findOne(row[i].nextBook, function (err, row2) {
-                    console.log("j = " + j);
-                    if (row2[0].nextBook != 0) {
-                        console.log(i);
-                        console.log("Test 3");
-                        Booking.findOne(row2[j].nextBook, function (err, row3) {
-                            toPass[j] = [row[j], row2[0], row3[0]]
 
-                    }
-                    else {
-                        toPass[j] = [row[i], row2[0]];
-                    }
-                }
-            }
-            else {
-                toPass[i] = [row[i-1]];
+    if(req.isAuthenticated() && req.user.user_type == 2 && req.user.auth_status == 1){
+        Booking.query(function (err, result) {
+            res.render('managerbookings', {shouldDisplayLogin: 2, result: result});
 
-            }
-        }
+        })
     }
-    console.log(toPass)
-    var toSend = [];
-    for(var r =0; r < toPass.length; r++){
-        for(var l = 0; l<toPass[l].length; i++){
 
-            Flight.findById(toPass[r][l].flightID, function (err, result) {
-                toSend[r][l] = result;
-            });
-        }
+
+    else if(req.isAuthenticated() && req.user.user_type == 1){
+            Booking.query(function (err, result) {
+                res.render('managerbookings', {shouldDisplayLogin: 2, result: result});
+
+            })
     }
-    res.render('bookingInfo', {shouldDisplayLogin: 2, result: toSend});
-
+    else {
+        Booking.findOne(req.passport.user.id, function (err,result) {
+            res.render('bookingInfo', {shouldDisplayLogin: 2, result: result});
+        })
+    }
 });
 
-});
 module.exports = router;

5fad87c8f7c0944de46c6b153d6f83eba4a793a1
emmahawk <katarina.hawk@gmail.com>
2017-05-05 01:54:21 -0500

    forms are flowing properly, but still need to pull price tiers

--- routes/purchase.js
+++ routes/purchase.js
@@ -1,13 +1,61 @@
 var express = require('express');
 var router = express.Router();
 
+var Flight = require('../src/models/flight');
+
 router.post('/', function (req,res) {
     // TODO: Get the booking information and stuff
     // This is the previous way we booked things; I moved it out here for your reference.
     // Some of it (like rendering the second flight search page) can be entirely ignored.
-    /*
-     router.post('/book', function (req,res) {
+    var flights = JSON.parse(req.body.booking_flights);
+    console.log(flights.length);
+    console.log(flights[0]);
+    console.log(flights[1]);
+    console.log('where the fuck are we?');
 
+    //query the database for the flights and grab the price
+    Flight.findPrices(flights, function (err, result) {
+        console.log('we are querying');
+        if (err) {
+            console.log(err);
+            throw err;
+        } else if (result.length > 0) {
+            console.log('we found that shit');
+            var priceTotal = 0;
+            for(var i =0; i < result.length; i++){
+                priceTotal += result[i].flight_basePrice;
+                console.log(priceTotal);
+            }
+            console.log('we are about to render the page');
+            res.render('purchaseoptions', {
+                shouldDisplayLogin: 2,
+                cost: priceTotal
+            });
+        }
+    });
+
+});
+
+//this is where the user can input their card information
+router.post('/submitoptions', function(req,res) {
+    console.log('were submitting optionssss');
+
+    //calculating the price
+    //var flightNumbers = req.body.flights;
+
+
+    res.render('payment', {
+        shouldDisplayLogin: 2
+        //cost: cost
+    });
+
+
+});
+
+//this is where the receipt generation will happen
+router.post('/confirm', function (req,res) {
+    console.log('we confirmed? the purchase..');
+    /*
      var flights = JSON.parse(req.body.booking_flights)
      var newBook = new Booking();
      newBook.userID =req.user.id;
@@ -111,6 +159,8 @@ router.post('/', function (req,res) {
      }
      });
      */
+
+
 });
 
 module.exports = router;
